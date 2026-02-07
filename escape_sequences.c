f21eac2c78075a357129791481f50bb08af256d3
k25-b <k256572@nu.edu.pk>
2026-02-07 16:48:19 +0500

    Create escape_sequences.c

---
+++ escape_sequences.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Task 6: Using Escape Sequences (\n for new line, \t for tab space)
+    
+    // Replace the placeholders below with your actual details
+    printf("Name:\t\tShaheer\n");
+    printf("Roll No:\t25K-6572\n");
+    printf("Section:\tB\n");
+
+    return 0;
+}
